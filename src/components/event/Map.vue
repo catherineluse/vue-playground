f3df952020153785dc992aa11ee372427b16919f
Catherine Luse <catherine.luse@gmail.com>
2021-10-13 21:54:18 -0700

    Use @fawmi/vue-google-maps

--- src/components/event/Map.vue
+++ src/components/event/Map.vue
@@ -1,33 +1,13 @@
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useGeolocation } from "./useGeolocation";
-import { GoogleMap, Marker } from "vue3-google-map";
-import config from "../../config";
-import { Point } from "../../types/eventTypes";
+import { defineComponent } from "vue";
 import places from "../../testData/places";
-
-const GOOGLE_MAPS_API_KEY: string = config.googleMapsApiKey as string;
 
 export default defineComponent({
   setup() {
-    const { coords } = useGeolocation();
-    const currentPosition = computed(
-      () =>
-        ({
-          lat: coords.value.latitude,
-          lng: coords.value.longitude,
-        } as Point)
-    );
     return {
-      currentPosition,
       center: { lat: 33.4255, lng: -111.94 },
-      GOOGLE_MAPS_API_KEY,
       places,
     };
-  },
-  components: {
-    GoogleMap,
-    Marker,
   },
 });
 </script>
@@ -35,20 +15,26 @@ export default defineComponent({
 <template>
   <div>Map</div>
   <div>
-    <p>Your position:</p>
-    <p>Latitude: {{ currentPosition.lat }}</p>
-    <p>Longitude: {{ currentPosition.lng }}</p>
-    <GoogleMap
-      :api-key="GOOGLE_MAPS_API_KEY"
-      style="width: 100%; height: 500px"
+    <GMapMap
       :center="center"
-      :zoom="15"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 100vw; height: 20rem"
     >
-      <Marker
-        v-for="place in places"
-        :key="place.Name"
-        :options="{ position: { lat: place.Latitude, lng: place.Longitude } }"
-      />
-    </GoogleMap>
+      <GMapCluster :zoomOnClick="true">
+        <GMapMarker 
+          v-for="place in places"
+          :key="place.Name"
+          :position="{ lat: place.Latitude, lng: place.Longitude }"
+          :clickable="true"
+          :draggable="true"
+          @click="center = { lat: place.Latitude, lng: place.Longitude }"
+        >
+          <GMapInfoWindow>
+            {{ place.Name }}
+          </GMapInfoWindow>
+        </GMapMarker>
+      </GMapCluster>
+    </GMapMap>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from './router';
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 import config from './config';
 import "./index.css";
 
 const app = createApp(App)
     .use(router)
+    .use(VueGoogleMaps, {
+        load: {
+            key: config.googleMapsApiKey
+        }
+    })
     .mount("#app");
